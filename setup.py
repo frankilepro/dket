417e8aa5a2f478f1e67c757a8ef9ef176b81a192
Francis Granger <franki_le_pro@hotmail.com>
2019-11-12 14:04:39 -0500

    comment deps links

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(name='dket',
       packages=find_packages(exclude=["tests"]),
       install_requires=[
           'editdistance==0.3.1',
-          'liteflow==0.1.0',
+        #   'liteflow==0.1.0',
           'six==1.10.0',
           'edit-distance==1.0.1'
           
       ],
-      dependency_links=[
-          'git+https://github.com/petrux/LiTeFlow.git@master#egg=liteflow-0.1.0'
-      ],
+    #   dependency_links=[
+    #       'git+https://github.com/petrux/LiTeFlow.git@master#egg=liteflow-0.1.0'
+    #   ],
       zip_safe=False)
