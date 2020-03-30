3b2d72b1b2b635b63e24e1959ea0fa1efaeb13e9
Francis Granger <franki_le_pro@hotmail.com>
2020-03-30 16:19:28 -0400

    lets test this out

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -13,7 +13,7 @@ TOKENIZER=$SCRIPTS/tokenizer/tokenizer.perl
 LC=$SCRIPTS/tokenizer/lowercase.perl
 CLEAN=$SCRIPTS/training/clean-corpus-n.perl
 BPEROOT=subword-nmt/subword_nmt
-BPE_TOKENS=1000
+BPE_TOKENS=5000
 
 URL="https://raw.githubusercontent.com/frankilepro/dket/master/datasets/def-form.tgz"
 GZ=def-form.tgz
